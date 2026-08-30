cd595a8c61d8de67c9f66f229e8de29462ee21be
Aswin C <aswinanand9020@gmail.com>
2026-08-30 13:23:21 +0530

    fix(app): opt in to Media3 cast API
    Mark Cast initialization with the required unstable API opt-in so Android lint can validate the application startup path.

--- app/src/main/java/cx/aswin/boxlore/BoxLoreApplication.kt
+++ app/src/main/java/cx/aswin/boxlore/BoxLoreApplication.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import androidx.media3.cast.Cast
+import androidx.media3.common.util.UnstableApi
 import androidx.work.Configuration
 import com.google.android.gms.cast.SessionState
 import com.google.android.gms.cast.framework.CastContext
@@ -159,6 +160,7 @@ class BoxLoreApplication :
                 .setWorkerFactory(LegacyWorkerFactory())
                 .build()
 
+    @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
 
